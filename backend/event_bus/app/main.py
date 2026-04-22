78e1daeb4184723bea86274b039552cba21b87b4
Thomas Mitchell <thomaswmitch@dhcp-10-12-148-240.dhcp.nd.edu>
2026-04-21 23:16:02 -0400

    url

--- backend/event_bus/app/main.py
+++ backend/event_bus/app/main.py
@@ -109,6 +109,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://main.d13pchhw5oxj1s.amplifyapp.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
